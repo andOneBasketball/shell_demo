db6b0adf7e3c8f8be212d57b52fe1271b29839d1
andOneBasketball <hhhhk00791@gmail.com>
2025-06-07 21:01:48 +0800

    update

--- kafka/kafka_basic_operation.sh
+++ kafka/kafka_basic_operation.sh
@@ -3,6 +3,8 @@
 # 查看 kafka 的所有 topic
 /usr/local/kafka_2.1-2.4.0-SNAPSHOT/bin/kafka-topics.sh --zookeeper kafka-zookeeper-1.novalocal,kafka-zookeeper-2.novalocal,kafka-zookeeper-3.novalocal --list
 
+/path/to/kafka/bin/kafka-topics.sh --version
+
 # 删除 topic
 ./kafka-topics.sh --zookeeper kafka-zookeeper-1.novalocal,kafka-zookeeper-2.novalocal,kafka-zookeeper-3.novalocal --delete --topic hello
 
@@ -11,6 +13,9 @@
 
 # 查看 kafka 历史消息
 ./kafka-console-consumer.sh --bootstrap-server kafka-zookeeper-1.novalocal,kafka-zookeeper-2.novalocal,kafka-zookeeper-3.novalocal --topic hello --group hk-68422 --from-beginning --property print.timestamp=true
+
+# 查看 group 的协调者
+kafka-consumer-groups.sh --bootstrap-server localhost:9092 --describe --group my-group
 
 # 发送消息
 ./bin/kafka-console-producer.sh --topic hids-file-info-check --broker-list kafka-1.novalocal:9092,kafka-2.novalocal:9092,kafka-3.novalocal:9092
